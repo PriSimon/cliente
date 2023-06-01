4564ed333ddbe0c9d2fbad51b46823f8ef6fe096
Priscila Simon <psimondev@gmail.com>
2023-06-01 16:42:17 -0300

    att index via vscode

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+	    <meta name="viewport" content="width=device-width, initial-scale=1">
+	    <link rel="stylesheet" type="text/css" href="css/principal.css">
+	    <title>Sistema C&P</title>
+        <meta name="author" content="Priscila Simon">
+	    <meta name="keywords" content="cadastro, cliente, produto, banco de dados">
+	    <meta name="description" content="Sistema para cadastro de clientes e produtos">
+    </head>
+<body>
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
